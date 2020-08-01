b7d1b0177d66e03c4df03b83fee947a68563bc4c
Ahmed Shan <ahmedshan16@gmail.com>
2020-08-01 05:17:16 +0500

    File renamed with ,py to .py
    Corrected syntax for print() function
    
    Added URL as a method to get_weather function

--- weather.py
+++ weather.py
@@ -2,8 +2,7 @@ import requests
 import datetime
 import time
 
-def get_weather():
-    url = 'URL WITH API'
+def get_weather( string: url ):
     weather_request = requests.get(url)
     weather_json = weather_request.json()
 
@@ -14,19 +13,21 @@ def get_weather():
     temp_min = weather_json['main']['temp_min']
     temp_max = weather_json['main']['temp_max']
     wind_speed = weather_json['wind']['speed']
-#    wind_degree = weather_json['wind']['deg']
+    wind_degree = weather_json['wind']['deg']
     sunrise = weather_json['sys']['sunrise']
     sunset = weather_json['sys']['sunset']
 
-#coverting timestamp to time
+    #coverting timestamp to time
     sundriseTime = datetime.datetime.fromtimestamp(sunrise).strftime('%H:%M:%S')
     sunsetTime = datetime.datetime.fromtimestamp(sunset).strftime('%H:%M:%S')
 
-    print 'Weather: ' +  str(description)
-    print 'Humidity: ' + str(humidity)
-    print 'Minimum Temperature: ' + str(temp_min)
-    print 'Maximum Temperature: ' + str(temp_max)
-    print 'Wind Speed: ' + str (wind_speed)
-#    print 'Wind Degree: ' + str(wind_degree)
-    print 'Sunrise: ' + str(sundriseTime)
-get_weather()
+    print('Weather: ' +  str(description))
+    print('Humidity: ' + str(humidity))
+    print('Minimum Temperature: ' + str(temp_min))
+    print('Maximum Temperature: ' + str(temp_max))
+    print('Wind Speed: ' + str (wind_speed))
+#    print('Wind Degree: ' + str(wind_degree))
+    print('Sunrise: ' + str(sundriseTime))
+
+# Run the function
+get_weather( 'https://something.com' )
